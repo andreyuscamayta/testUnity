ab11dc6a80da01d10f858ff13b02f1e0d14d8845
Andrey Omar Mozo Uscamayta <omar2562@gmail.com>
2021-05-07 11:02:46 -0300

    move variables

---
+++ Assets/Script/move.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*Debug.Log("deltaTime"+Time.deltaTime);
+        Debug.Log("Input.GetAxis(Vertical)" + Input.GetAxis("Vertical"));
+        Debug.Log("Input.GetAxis(Horizontal)" + Input.GetAxis("Horizontal"));
+        Debug.Log("Input.GetAxis(Mouse X)" + Input.GetAxis("Mouse X"));
+        Debug.Log("Input.GetAxis(Mouse Y)" + Input.GetAxis("Mouse Y"));
+        Debug.Log("transform.position" + transform.position);
+        Debug.Log("transform.rotation" + transform.rotation);
+        Debug.Log("transform.localScale" + transform.localScale);
+        transform.localScale+= new Vector3(-0.01f, -0.01f, -0.01f); ;*/
+    }
+}
